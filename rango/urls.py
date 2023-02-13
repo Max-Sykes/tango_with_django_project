481875e28d8f74874e93f4b32430cc456766d9f5
“Max <2686894s@student.gla.ac.uk>
2023-02-13 21:16:13 +0000

    Chapter 4 Tasks had syntax error, may amend later

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,7 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-path('', views.index, name='index'),
-path('about/', views.about, name='about'),
+path('', views.index, name='index.html'),
+path('about/', views.about, name='about.html'),
 
 ]
--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>Index</a>")
+
+        context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+        return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says her is the about page.  <a href='/rango/'>Index</a>")
+        return render(request, 'rango/about.html')
