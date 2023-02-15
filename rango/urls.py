2fbf0672b2585df7f6ff4370f62c52f7408cc4b6
“Max <2686894s@student.gla.ac.uk>
2023-02-15 16:53:50 +0000

    Chapter 6 exercises, index not being seen as a view error?

--- rango/urls.py
+++ rango/urls.py
@@ -5,7 +5,8 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-path('', views.index, name='index.html'),
-path('about/', views.about, name='about.html'),
 
+    path('', views.index, name='index.html'),
+    path('about/', views.about, name='about.html'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
 ]
